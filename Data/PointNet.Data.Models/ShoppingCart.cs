7c3c0c3262c37b7f024ff6317737b765bae6a428
Stoyan Georgiev <stoyan_online@abv.bg>
2020-04-21 21:35:18 +0300

    Added data validation (client and server sides)  and TEST project

--- Data/PointNet.Data.Models/ShoppingCart.cs
+++ Data/PointNet.Data.Models/ShoppingCart.cs
@@ -13,31 +13,13 @@
     public class ShoppingCart : BaseDeletableModel<int>
     {
 
-        //public ShoppingCart()
-        //{
-        //    ShoppingCartItems = new List<ShoppingCartItem>();
-        //}
-
         public ShoppingCart()
         {
 
         }
 
-        //public ShoppingCart(IRepository<ShoppingCartItem> shoppingCartItemRepository)
-        //{
-        //    this.shoppingCartItemRepository = shoppingCartItemRepository;
-        //}
-
         public string UserId { get; set; }
+
         public List<ShoppingCartItem> ShoppingCartItems { get; set; }
-
-        //public List<ShoppingCartItem> GetShoppingCartItems()
-        //{
-        //    return ShoppingCartItems ??
-        //        (ShoppingCartItems =
-        //        shoppingCartItemRepository.All().Where(c => c.ShoppingCartId == Id).Include(s => s.Product).ToList());
-        //}
-
-
     }
 }
--- Web/PointNet.Web.ViewModels/Catalog/ProductViewModel.cs
+++ Web/PointNet.Web.ViewModels/Catalog/ProductViewModel.cs
@@ -12,16 +12,27 @@
     {
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(50, ErrorMessage = "length between 2 and 50", MinimumLength = 2)]
         public string Title { get; set; }
 
+        [Required]
+        [DataType(DataType.MultilineText)]
         public string Description { get; set; }
 
+        [Required]
+        [DataType(DataType.ImageUrl)]
         public string ImageUrl { get; set; }
 
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity can not be negative or 0")]
         public int Quantity { get; set; }
 
+        [Required]
+        [DataType(DataType.Currency)]
+        [Range(1, int.MaxValue, ErrorMessage = "Price can not be negative or 0")]
         public decimal Price { get; set; }
 
+        [Required]
         [Display(Name = "Category")]
         public int CategoryId { get; set; }
 
